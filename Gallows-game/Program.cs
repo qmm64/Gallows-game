6a23bcf9193cd82ed3200e0c814f47feeb264ad6
qmm64 <10gmagma01@gmail.com>
2025-05-23 20:06:11 +0700

    First commit

---
+++ Gallows-game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gallows_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
