6918710b2a84d8597719a8cf5f9de114fbbcefbd
qmm64 <10gmagma01@gmail.com>
2025-05-23 21:18:09 +0700

    Create WordLoader

---
+++ Gallows-game/WordLoader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallows_game
+{
+    internal class WordLoader
+    {
+        private List<string> _words;
+        private string _path;
+
+        public WordLoader() 
+        {
+            _path = "../../../../Files/Words.txt";
+            _words = ParseWords(ReadFile(_path));
+        }
+
+        private string ReadFile(string path)
+        {
+            try
+            {
+                string text;
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    text = reader.ReadToEnd();
+                }
+                return text;
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"Ошибка чтения файла. Текст ошибки: {ex.Message}");
+                Environment.Exit(1);
+                return null;
+            }
+        }
+
+        private List<string> ParseWords(string text)
+        {
+            try
+            {
+                StringBuilder word = new StringBuilder();
+                List<string> words = new List<string>();
+                words = text.Split(", ").ToList();
+                return words;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка чтения слов из файла. Текст ошибки: {ex.Message}");
+                Environment.Exit(1);
+                return null;
+            }
+        }
+
+        public string GetWord()
+        {
+            try
+            {
+                Random random = new Random();
+                string word = _words[random.Next(_words.Count)];
+                return word;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Ошибка получения случайного слова. Текст ошибки: {ex.Message}");
+                Environment.Exit(1);
+                return null;
+            }
+        }
+    }
+}
